06369683c6a87565aa9a26ca2ba340d2c93d25fa
mizuki <banana0822hairuyo@gmail.com>
2023-05-22 20:07:24 +0900

    2023/05/22 20:07

--- marubatu.py
+++ marubatu.py
@@ -9,6 +9,13 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 pygame.display.set_caption('〇×ゲーム')
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+LINE_WIDTH = 5
+
+display_surface.fill(BLACK)
+
 running = True
 
 while running:
