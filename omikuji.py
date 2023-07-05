e918097c10771b2b4faaa04458664731800a4993
mizuki <banana0822hairuyo@gmail.com>
2023-07-05 12:46:56 +0900

    2023/07/05 12:46

--- omikuji.py
+++ omikuji.py
@@ -9,6 +9,12 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 pygame.display.set_caption("おみくじ")
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+
+display_surface.fill(BLACK)
+
 running = True
 while running:
     for event in pygame.event.get():
