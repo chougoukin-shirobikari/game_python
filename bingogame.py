29aac4d56bef2c6d55630651edcabb14329bc287
mizuki <banana0822hairuyo@gmail.com>
2024-01-24 16:31:03 +0900

    2024/01/24 16:30

--- bingogame.py
+++ bingogame.py
@@ -26,6 +26,19 @@ clock = pygame.time.Clock()
 class Bingo():
     def __init__(self):
         pygame.draw.rect(display_surface, WHITE, (10, 150, 390, 390), 1)
+        pygame.draw.rect(display_surface, WHITE, (425, 200, 150, 150), 1)
+        pygame.draw.rect(display_surface, WHITE, (410, 425, 180, 225), 1)
+
+        self.number_button = pygame.draw.rect(display_surface, WHITE, (30, WINDOW_HEIGHT - 125, 350, 80), 1)
+
+        text_board('NotoSansJP-Regular.ttf', 50, 'ビンゴゲーム', YELLOW, None, WINDOW_WIDTH // 2, 50)
+    
+def text_board(font, size, text, color, bgColor, x, y):
+    font = pygame.font.Font(font, size)
+    textsurf = font.render(text, True, color, bgColor)
+    textsurf_rect = textsurf.get_rect()
+    textsurf_rect.center = (x, y)
+    display_surface.blit(textsurf, textsurf_rect)
 
 bingo = Bingo()
 
