20c9ad37d119100a598bb1b3800775e702874e41
mizuki <banana0822hairuyo@gmail.com>
2024-01-28 22:33:47 +0900

    2024/01/28 22:32

---
+++ meirogame.py
@@ -0,0 +1,44 @@
+import pygame
+
+
+pygame.init()
+
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
+
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+pygame.display.set_caption("迷路")
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+GRAY = (128, 128, 128)
+LemonChiffon = (255, 250, 205)
+BROWN = (165, 42, 22)
+STEELBLUE = (70, 130, 180)
+
+display_surface.fill(BLACK)
+
+FPS = 60
+clock = pygame.time.Clock()
+
+class Maze():
+    def __init__(self):
+        self.x = 70
+        self.y = 100
+
+maze = Maze()
+
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    clock.tick(FPS)
+
+pygame.quit()
