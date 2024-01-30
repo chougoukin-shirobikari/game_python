ac2fb4ddb385d142f40873c41d1275d3fca44c8a
mizuki <banana0822hairuyo@gmail.com>
2024-01-30 23:01:59 +0900

    2024/01/30 23:01

--- meirogame.py
+++ meirogame.py
@@ -1,4 +1,5 @@
-import pygame
+import pygame, random
+from mazeResource import maze_list
 
 
 pygame.init()
@@ -16,7 +17,7 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 GRAY = (128, 128, 128)
 LemonChiffon = (255, 250, 205)
-BROWN = (165, 42, 22)
+BROWN = (165, 42, 42)
 STEELBLUE = (70, 130, 180)
 
 display_surface.fill(BLACK)
@@ -24,10 +25,38 @@ display_surface.fill(BLACK)
 FPS = 60
 clock = pygame.time.Clock()
 
+def text_board(font, size, text, color, bgColor, x, y):
+    font = pygame.font.Font(font, size)
+    textsurf = font.render(text, True, color, bgColor)
+    textsurf_rect = textsurf.get_rect()
+    textsurf_rect.center = (x, y)
+    display_surface.blit(textsurf, textsurf_rect)
+
 class Maze():
     def __init__(self):
         self.x = 70
         self.y = 100
+        
+        self.createMaze()
+    
+    def createMaze(self):
+        text_board('NotoSansJP-Regular.ttf', 50, '迷路', WHITE, None, WINDOW_WIDTH // 2, 50)
+
+        choosed_maze = random.choice(maze_list)
+
+        for y in range(15):
+            for x in range(15):
+                if choosed_maze[y][x] == 'o':
+                    pygame.draw.rect(display_surface, WHITE, (self.x + 30 * x, self.y + 30 * y, 30, 30))
+                elif choosed_maze[y][x] == 'x':
+                    pygame.draw.rect(display_surface, GRAY, (self.x + 30 * x, self.y + 30 * y, 30, 30))
+                elif choosed_maze[y][x] == 'G':
+                    pygame.draw.rect(display_surface, RED, (self.x + 30 * x, self.y + 30 * y, 30, 30))
+                elif choosed_maze[y][x] == 'Y':
+                    pygame.draw.rect(display_surface, YELLOW, (self.x + 30 * x, self.y + 30 * y, 30, 30))
+                elif choosed_maze[y][x] == 'B':
+                    pygame.draw.rect(display_surface, BROWN, (self.x + 30 * x, self.y + 30 * y, 30, 30))
+
 
 maze = Maze()
 
@@ -38,6 +67,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    pygame.display.update()
     
     clock.tick(FPS)
 
