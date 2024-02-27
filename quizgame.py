635e1f98fd317061116db2d2d25716ba23314c5c
mizuki <banana0822hairuyo@gmail.com>
2024-02-27 12:47:04 +0900

    2024/02/27 12:46

--- quizgame.py
+++ quizgame.py
@@ -1,4 +1,5 @@
-import pygame
+import pygame, random
+from quizResource import quiz
 
 
 pygame.init()
@@ -28,9 +29,35 @@ class Game():
         self.width = width
         self.height = height
         self.line = 1
+
+        self.questionAnswer = -1
+        self.answer1 = self.answer2 = self.answer3 = self.answer4 = self.answerDescription = None
     
     def draw(self):
         pygame.draw.rect(display_surface, self.color, (self.x, self.y, self.width, self.height), self.line)
+
+    def theQuestion(self):
+        number = 0
+        number = random.randrange(len(quiz))
+
+        question, self.answer1, self.answer2, self.answer3, self.answer4, self.questionAnswer, self.answerDescription = quiz[number]
+        
+        self.text_board('NotoSansJP-Regular.ttf', 30, question, WHITE, None, WINDOW_WIDTH // 2, 100)
+        self.text_board('NotoSansJP-Regular.ttf', 30, self.answer1, WHITE, None, WINDOW_WIDTH // 2, 240)
+        self.text_board('NotoSansJP-Regular.ttf', 30, self.answer2, WHITE, None, WINDOW_WIDTH // 2, 340)
+        self.text_board('NotoSansJP-Regular.ttf', 30, self.answer3, WHITE, None, WINDOW_WIDTH // 2, 440)
+        self.text_board('NotoSansJP-Regular.ttf', 30, self.answer4, WHITE, None, WINDOW_WIDTH // 2, 540)
+    
+    def text_board(self, font, size, text, color, bgColor, x, y):
+        font = pygame.font.Font(font, size)
+        textsurf = font.render(text, True, color, bgColor)
+        textsurf_rect = textsurf.get_rect()
+        textsurf_rect.center = (x, y)
+
+        display_surface.blit(textsurf, textsurf_rect)
+
+    def questionAnswerCheck(self):
+        return self.questionAnswer - 1
 
 class Player(Game):
     def __init__(self, x, y):
@@ -50,9 +77,13 @@ class Player(Game):
         elif answer_choice == 1 and self.answer < 3:
             pygame.draw.rect(display_surface, WHITE, (self.x, self.y, self.width, self.height), self.line)
             self.answer += 1
+    
+    def answerCheck(self):
+        return self.answer
 
 game = Game(10, 20, WINDOW_WIDTH - 20, 160)
 game.draw()
+game.theQuestion()
 
 answer1, answer2, answer3, answer4 = Game(50, 200), Game(50, 300), Game(50, 400), Game(50, 500)
 answers = [answer1, answer2, answer3, answer4]
@@ -72,6 +103,12 @@ while running:
                 player.move(-1)
             if event.key == pygame.K_DOWN:
                 player.move(1)
+            if event.key == pygame.K_SPACE:
+                print(player.answerCheck(), game.questionAnswerCheck())
+                if player.answerCheck() == game.questionAnswerCheck():
+                    print('正解')
+                else:
+                    print('不正解')
     
     player.draw()
     
