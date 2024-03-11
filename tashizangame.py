f0c9f216bbb23df990c187673451eb2245c9e111
mizuki <banana0822hairuyo@gmail.com>
2024-03-12 00:40:53 +0900

    2024/03/12 0:40

--- tashizangame.py
+++ tashizangame.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 
 
 pygame.init()
@@ -33,15 +33,60 @@ def text_board(font, size, text, color, x, y):
 text_board('NotoSansJP-Regular.ttf', 50, '足し算ゲーム', WHITE, WINDOW_WIDTH // 2, 50)
 
 class Game():
+    CHOICE_X = 25
+    CHOICE_Y = 305
+    CHOICE_PADDING = 150
+
+    CHOICE_WIDTH = 100
+    CHOICE_HEIGHT = 150
+
+    LINE_WIDTH = 3
+
     def __init__(self):
-        self.lineCreate()
+        self.choice1, self.choice2, self.choice3, self.choice4 = self.lineCreate()
+
+        self.a = random.randrange(1, 5)
+        self.b = random.randrange(1, 6)
+
+        self.add()
+
+        self.answer = self.a + self.b
+
+        self.choice_number = []
+
+        self.count = 0
+        self.best_count = 0
+
+        self.countTextBoard()
+
+        self.choiceNumber()
+    
+    def countTextBoard(self):
+        text_board('NotoSansJP-Regular.ttf', 30, f'連続正解数：{self.count}', WHITE, WINDOW_WIDTH // 2, WINDOW_HEIGHT - 100)
+        text_board('NotoSansJP-Regular.ttf', 30, f'今回の最高連続正解数：{self.best_count}', WHITE, WINDOW_WIDTH // 2, WINDOW_HEIGHT - 50)
     
     def lineCreate(self):
         pygame.draw.rect(display_surface, WHITE, (50, 120, 500, 150), 3)
-        pygame.draw.rect(display_surface, WHITE, (25, 305, 100, 150), 3)
-        pygame.draw.rect(display_surface, WHITE, (175, 305, 100, 150), 3)
-        pygame.draw.rect(display_surface, WHITE, (325, 305, 100, 150), 3)
-        pygame.draw.rect(display_surface, WHITE, (475, 305, 100, 150), 3)
+
+        return [pygame.draw.rect(display_surface, WHITE, (self.CHOICE_X + self.CHOICE_PADDING * c, self.CHOICE_Y, self.CHOICE_WIDTH, self.CHOICE_HEIGHT), self.LINE_WIDTH) for c in range(4)]
+    
+    def add(self):
+        text_board('NotoSansJP-Regular.ttf', 100, f'{self.a} + {self.b}', WHITE, WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 - 100)
+    
+    def choiceNumber(self):
+        while len(self.choice_number) < 3:
+            temp_number = random.randrange(2, 10)
+            if temp_number not in self.choice_number:
+                self.choice_number.append(temp_number)
+        
+        self.choice_number.insert(random.randrange(4), self.answer)
+
+        for c in range(4):
+            text_board('NotoSansJP-Regular.ttf', 100, f'{self.choice_number[c]}', WHITE, (c * self.CHOICE_PADDING) + self.CHOICE_X + 50, self.CHOICE_Y + 70)
+
+    def clickCheck(self, playerChoiceIndex):
+        print(self.answer, self.choice_number[playerChoiceIndex])
+        print(self.answer == self.choice_number[playerChoiceIndex])
 
 
 game = Game()
@@ -53,6 +98,17 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                x, y = event.pos
+                if game.choice1.collidepoint(x, y):
+                    game.clickCheck(0)
+                if game.choice2.collidepoint(x, y):
+                    game.clickCheck(1)
+                if game.choice3.collidepoint(x, y):
+                    game.clickCheck(2)
+                if game.choice4.collidepoint(x, y):
+                    game.clickCheck(3)
     
     pygame.display.update()
     
