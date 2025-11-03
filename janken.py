b6d8f68a6d0cdd5d1fc77b5cbf8f0a0573d525e9
mizuki <banana0822hairuyo@gmail.com>
2025-11-03 19:04:53 +0900

    2025/11/03 19:04

---
+++ janken.py
@@ -0,0 +1,38 @@
+import pygame
+
+
+pygame.init()
+
+WINDOW_WIDTH = 700
+WINDOW_HEIGHT = 500
+
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+pygame.display.set_caption("じゃんけんゲーム")
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+GRAY = (128, 128, 128)
+
+FPS = 60
+clock = pygame.time.Clock()
+
+font = pygame.font.SysFont('meiryo', 50)
+textsurf = font.render("何を選びますか？", True, WHITE)
+textsurf_rect = textsurf.get_rect()
+textsurf_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT - 150)
+
+display_surface.blit(textsurf, textsurf_rect)
+
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.display.update()
+    
+    clock.tick(FPS)
