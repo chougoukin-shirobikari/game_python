9722b2b72db4054397a80dff30d4a90b204762d2
mizuki <banana0822hairuyo@gmail.com>
2025-11-23 21:59:45 +0900

    2025/11/23 21:59

--- bingogame.py
+++ bingogame.py
@@ -36,16 +36,24 @@ class Bingo():
         text_board('NotoSansJP-Regular.ttf', 30, '出た数字', SILVER, None, 500, 390)
         text_board('NotoSansJP-Regular.ttf', 40, '番号を選ぶ', WHITE, None, 208, WINDOW_HEIGHT - 85)
 
+        self.bingo_list = []
+
         self.bingo_display()
+
+        self.bingo_number_gray_display = {}
         
         self.record_the_bingo_numbers()
 
         self.not_choosed_numbers = product_bingo_numbers()
+
+        self.bingo_game = [[False] * 5 for _ in range(5)]
+        self.bingo_game[2][2] = True
     
     def bingo_display(self):
         bingo_numbers = product_bingo_numbers()
 
         for y in range(5):
+            temp_bingo_list = []
             for x in range(5):
                 number = str(bingo_numbers.pop(random.randrange(len(bingo_numbers))))
                 rect = pygame.draw.rect(display_surface, WHITE, (20 + 75 * x, 160 + 75 * y, 70, 70), 1)
@@ -56,6 +64,10 @@ class Bingo():
                 
                 else:
                     text_board('NotoSansJP-Regular.ttf', 30, number, WHITE, None, rect.centerx, rect.centery)
+                
+                temp_bingo_list.append([rect, number])
+            
+            self.bingo_list.append(temp_bingo_list)
     
     def change_bingo_number(self, rect, num, rect_color=LemonChiffon, num_color=GRAY):
         pygame.draw.rect(display_surface, rect_color, (rect.x + 1, rect.y + 1, rect.width - 2, rect.height - 2))
@@ -69,6 +81,44 @@ class Bingo():
                 if bingo_numbers:
                     number = bingo_numbers.pop(0)
                     text_board('NotoSansJP-Regular.ttf', 15, str(number), WHITE, None, 430 + 20 * x, 445 + 20 * y)
+
+                    self.bingo_number_gray_display[number] = ['NotoSansJP-Regular.ttf', 15, str(number), GRAY, None, 430 + 20 * x, 445 + 20 * y]
+
+    def check_the_game(self, number):
+        for y in range(len(self.bingo_list)):
+            for x in range(len(self.bingo_list[y])):
+                r, n = self.bingo_list[y][x]
+
+                if n == number:
+                    self.change_bingo_number(r, n)
+                    self.bingo_game[y][x] = True
+    
+    def check_bingo(self):
+        check = False
+
+        for y in range(5):
+            if all(self.bingo_game[y]):
+                check = True
+                for x in range(5):
+                    self.change_bingo_number(*self.bingo_list[y][x], YELLOW)
+        
+        for x in range(5):
+            if all(self.bingo_game[y][x] for y in range(5)):
+                check = True
+                for y in range(5):
+                    self.change_bingo_number(*self.bingo_list[y][x], YELLOW)
+        
+        if all(self.bingo_game[i][i] for i in range(5)):
+            check = True
+            for i in range(5):
+                self.change_bingo_number(*self.bingo_list[i][i], YELLOW)
+        
+        if all(self.bingo_game[i][4-i] for i in range(5)):
+            check = True
+            for i in range(5):
+                self.change_bingo_number(*self.bingo_list[i][4-i], YELLOW)
+        
+        return check
 
 def product_bingo_numbers():
     bingo_numbers = []
@@ -97,6 +147,9 @@ while running:
             running = False
         
         if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button != 1:
+                continue
+
             mouse_x, mouse_y = pygame.mouse.get_pos()
             
             if bingo.number_button.collidepoint(mouse_x, mouse_y):
@@ -104,6 +157,35 @@ while running:
 
                 pygame.draw.rect(display_surface, BLACK, (426, 201, 148, 148))
                 text_board('NotoSansJP-Regular.ttf', 100, str(number), WHITE, None, 500, 270)
+
+                text_board(*bingo.bingo_number_gray_display[int(number)])
+                
+                bingo.check_the_game(number)
+
+                check_bingo_result = bingo.check_bingo()
+
+                if check_bingo_result:
+                    text_board('NotoSansJP-Regular.ttf', 120, 'ビンゴ！', RED, None, WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 - 10)
+                    text_board('NotoSansJP-Regular.ttf', 30, 'スペースキーを押してもう一度ゲームをする', GRAY, WHITE, WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 189)
+
+                    pygame.display.update()
+
+                    game_over = True
+
+                    while game_over:
+                        bingo = None
+
+                        for event in pygame.event.get():
+                            if event.type == pygame.QUIT:
+                                game_over = False
+                                running = False
+                            
+                            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                                game_over = False
+
+                                display_surface.fill(BLACK)
+
+                                bingo = Bingo()
 
     pygame.display.update()
     
